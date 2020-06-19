170dbb9360a59c0f46dd55ef1a6aec72090c8dba
Tobias Weisskopf <tobias.weisskopf.ch@gmail.com>
2020-06-19 22:34:19 +0200

    text display in website

--- parseboard/main/routes.py
+++ parseboard/main/routes.py
@@ -14,11 +14,18 @@ def home():
     return render_template('home.html', articles=articles, issues=all_issues)
 
 
+# TODO: The user is unable to get the text back to the website...
 @main.route('/_report', methods=['GET', 'POST'])
 def reporting():
-    issue_id = request.args('issue', 0, type=int)
-    article_id = request.args('article', 0, type=int)
-
+    issue_id = request.args.get('issue', 0, type=int)
+    article_id = request.args.get('article', 0, type=int)
     issue = Issue.query.get(issue_id)
-    article = Article.query.get(article_id)
-    return jsonify({'isu': issue.serialize(), 'art': article.serialize()})
+
+    # Based on order in database... not ideal but ok for now
+    article_de = Article.query.get(article_id)
+    article_en = Article.query.get(article_id + 1)
+    article_fr = Article.query.get(article_id + 2)
+
+    print(article_de.title, article_en.title, article_fr.title)
+
+    return jsonify({'de': article_de.serialize(), 'en': article_en.serialize(), 'fr': article_fr.serialize()})
